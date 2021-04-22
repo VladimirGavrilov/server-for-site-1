5b097e0b87d778c65dcafde94c1d5ccc376248f1
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-22 11:40:01 +0300

    text output localserver

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./data/pesok.txt');
-const writeStream = fs.createWriteStream('./data/news.txt');
-
-readStream.on('data', function(chunk) {
-    console.log("Новые данные получены/n")
-    writeStream.write(chunk)
-})
-
 console.log("work");
+///подключение локального сервера
+const http = require('http');
+const server = http.createServer(function(req, res) {
+    console.log(`URL страницы ${req.url}`)
+    const readStream = fs.createReadStream('./data/pesok.txt');
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'})
+    readStream.pipe(res);
+   
+});
+server.listen(3000,'127.0.0.1')
+console.log('Мы отслеживаем port 3000')
 
