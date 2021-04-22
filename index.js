d1eb9e60a7eb00a8aeedc22120442dc6cbf61b74
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-22 11:21:26 +0300

    read file txt

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const fs = require('fs')
-const my_count = []
-console.log("work")
+const fs = require('fs');
+const readStream = fs.createReadStream('./data/pesok.txt');
+const writeStream = fs.createWriteStream('./data/news.txt');
+
+readStream.on('data', function(chunk) {
+    console.log("Новые данные получены/n")
+    writeStream.write(chunk)
+})
+
+console.log("work");
+
